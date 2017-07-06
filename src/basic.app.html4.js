58412ac3befc66d710e2f2fe741915ffcc523f2c
Jo Kerkhofs <jke@thinkerit.be>
2017-07-06 19:21:43 +0200

    Moved everything to start.app repo

--- src/basic.app.html4.js
+++
@@ -1,7 +0,0 @@
-angular.module('basic.app').config(['$locationProvider', '$provide', function ($locationProvider, $provide) {
-    $provide.decorator('$sniffer', ['$delegate', function ($delegate) {
-        $delegate.history = false;
-        return $delegate;
-    }]);
-    $locationProvider.html5Mode(false).hashPrefix('!');
-}]);
