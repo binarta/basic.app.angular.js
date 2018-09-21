52a3d9c475a87de91905480dd28ebcf2d66186d6
sgo <sgo@thinkerit.be>
2018-09-21 15:21:28 +0200

    Include publisher gateway bindings.

--- src/binarta-gateways-inmem.js
+++ src/binarta-gateways-inmem.js
@@ -5,23 +5,22 @@
     angular.module('binarta-checkpointjs-gateways-angular1', ['binarta-checkpointjs-inmem-angular1'])
         .provider('binartaCheckpointGateway', ['inmemBinartaCheckpointGatewayProvider', proxy]);
 
+    angular.module('binarta-publisherjs-gateways-angular1', ['binarta-publisherjs-inmem-angular1'])
+        .provider('binartaPublisherGateway', ['inmemBinartaPublisherGatewayProvider', proxy]);
+
     angular.module('binarta-gateways-angular1', [
         'binartajs-angular1',
         'binarta-applicationjs-gateways-angular1',
-        'binarta-checkpointjs-gateways-angular1'
+        'binarta-checkpointjs-gateways-angular1',
+        'binarta-publisherjs-gateways-angular1'
     ])
-        .run([
-            '$q',
-            'binartaGatewaysAreInitialised',
-            'binartaApplicationGateway',
-            IsInitialised
-        ]);
+        .run(['binartaGatewaysAreInitialised', 'binartaApplicationGateway', IsInitialised]);
 
     function proxy(gateway) {
         return gateway;
     }
 
-    function IsInitialised($q, gatewaysAreInitialised, applicationGateway) {
+    function IsInitialised(gatewaysAreInitialised, applicationGateway) {
         applicationGateway.addPublicConfig({
             id: 'application.pages.contact.active',
             value: 'true'
